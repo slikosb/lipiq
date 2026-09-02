f952f9e04e1e009307feac0e746ef26dce96341c
Clément Le Batard <clement.lebatard1@gmail.com>
2026-09-02 22:40:51 +0200

    problème de camera ajout autofocus

--- src/app/app.ts
+++ src/app/app.ts
@@ -27,6 +27,8 @@ export class App {
     BarcodeFormat.EAN_8,
     BarcodeFormat.UPC_A,
     BarcodeFormat.UPC_E,
+    BarcodeFormat.CODE_128,
+    BarcodeFormat.CODE_39,
   ];
 
   searchByBarcode(code?: string): void {
